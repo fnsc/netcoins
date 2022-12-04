84300e5048329866203d0e4fd0d427ceb7ad0b76
Gabriel Fonseca <gabrieldfnsc@gmail.com>
2022-12-03 19:22:16 -0800

    feat(challenge): add price controller test

---
+++ tests/Integration/Presenters/Http/Controllers/Api/PriceControllerTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Integration\Presenters\Http\Controllers\Api;
+
+use Crypto\Application\Contracts\Client;
+use Crypto\Application\Price\InputBoundary;
+use Crypto\Application\Price\Service;
+use Exception;
+use GuzzleHttp\Exception\BadResponseException;
+use Illuminate\Http\Response;
+use Mockery as m;
+use Tests\TestCase;
+use Throwable;
+
+class PriceControllerTest extends TestCase
+{
+    public function testShouldGetCryptoPrices(): void
+    {
+        // Set
+        $client = $this->instance(Client::class, m::mock(Client::class));
+        $queryParams = ['ids' => 'bitcoin', 'vs_currencies' => 'usd'];
+        $clientResponse = [
+            'bitcoin' => [
+                'usd' => 16961.09,
+            ],
+        ];
+
+        $expected = [
+            'data' => [
+                'id' => 'bitcoin',
+                'currency' => 'usd',
+                'value' => '16961.09',
+            ],
+        ];
+
+        // Expectations
+        /** @phpstan-ignore-next-line  */
+        $client->expects()
+            ->get('simple/price', $queryParams)
+            ->andReturn($clientResponse);
+
+        // Action
+        $result = $this->get('api/v1/crypto/price?crypto-currency=bitcoin');
+
+        // Assertions
+        $result->assertStatus(Response::HTTP_OK);
+        $this->assertSame($expected, $result->getOriginalContent());
+    }
+
+    public function testShouldReturnUnprocessableEntityWhenQueryParamNotSent(): void
+    {
+        // Set
+        $expected = [
+            'data' => [],
+            'errors' => [
+                'crypto-currency' => [
+                    'The crypto-currency field is required.',
+                ],
+            ],
+        ];
+
+        // Action
+        $result = $this->get('api/v1/crypto/price');
+
+        // Assertions
+        $result->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertSame($expected, $result->getOriginalContent());
+    }
+
+    /**
+     * @dataProvider getExceptionScenarios
+     * @param Throwable            $exception
+     * @param array<string, mixed> $expected
+     * @param int                  $expectedStatusCode
+     */
+    public function testShouldReturnAnError(Throwable $exception, array $expected, int $expectedStatusCode): void
+    {
+        // Set
+        $service = $this->instance(
+            Service::class,
+            $this->createMock(Service::class)
+        );
+        $input = new InputBoundary('bitcoin');
+
+        // Expectations
+        /** @phpstan-ignore-next-line  */
+        $service->expects($this->once())
+            ->method('handle')
+            ->with($input)
+            ->willThrowException($exception);
+
+        // Action
+        $result = $this->get('api/v1/crypto/price?crypto-currency=bitcoin');
+
+        // Assertions
+        $result->assertStatus($expectedStatusCode);
+        $this->assertSame($expected, $result->getOriginalContent());
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getExceptionScenarios(): array
+    {
+        return [
+            'guzzle exception' => [
+                'exception' => m::mock(BadResponseException::class),
+                'expected' => [
+                    'data' => [],
+                    'errors' => 'Error while receiving data from the third part API. Try again later.',
+                ],
+                'expectedStatusCode' => Response::HTTP_BAD_REQUEST,
+            ],
+            'unexpected exception' => [
+                'exception' => new Exception('Something unexpected.'),
+                'expected' => [
+                    'data' => [],
+                    'errors' => 'Something unexpected has happened. Try again later.',
+                ],
+                'expectedStatusCode' => Response::HTTP_INTERNAL_SERVER_ERROR,
+            ],
+        ];
+    }
+}
